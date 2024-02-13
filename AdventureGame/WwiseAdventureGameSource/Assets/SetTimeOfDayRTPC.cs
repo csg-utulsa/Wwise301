1bea74d5b4d77f66d3a553aac64604052491c5f5
netliu48 <netlau666@gmail.com>
2024-02-13 12:21:11 -0600

    Lesson 5
    - states using AKstate
    - game parameters with Wwise types

---
+++ AdventureGame/WwiseAdventureGameSource/Assets/SetTimeOfDayRTPC.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTimeOfDayRTPC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AK.Wwise.RTPC TimeOfDayRTPC;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
